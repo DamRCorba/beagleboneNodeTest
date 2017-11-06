b37ea52b7be5fb371146f87b68bb7972d60a0cfa
damian <damian@damian.damian>
2017-11-06 10:21:20 -0300

    Se agraga parpadeo

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require("express");
 
-var app = express(); 
+var app = express();
 
 var b = require('bonescript');
 var led = "USR3";
 var state = 0;
+var Intervalo;
 
 app.set("view engine", "jade");
 
@@ -15,7 +16,7 @@ app.get("/",function(req,res){
 app.post("/encender",function(req,res){
 	b.pinMode(led, 'out');
 	b.digitalWrite(led, 1);
-	res.render("encender");	
+	res.render("encender");
 });
 
 app.post("/apagar",function(req,res){
@@ -23,9 +24,27 @@ app.post("/apagar",function(req,res){
 	b.digitalWrite(led,0);
 	res.render("apagar");
 });
-	
 
+app.post("/blink",function(req,res){
+	Intervalo = setTimeout(blink, 500);
+	res.render("botoncito");
+});
 
+app.post("/stop",function(req,res){
+	 clearTimeout(Intervalo);
+	 res.render("botoncito");
+});
+
+function  blink(){
+	if(!state)
+		{
+			b.digitalWrite(led,1);
+			state = 1;
+		}
+		else{
+			b.digitalWrite(led,0);
+			state = 0;
+		}
+}
 
 app.listen(8989); //tambien puede darse un host y callback
-
